4f5621bf3e249c4ba71d09da82a6bc63c0c0a76d
Josh <13joshmmartin@gmail.com>
2020-07-24 19:36:05 -0400

    finished implementing everything

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Loading from "./Loading";
+import "./index.css";
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
+  state = { lat: null, errorMessage: "" };
 
-    return <div> Latitude:</div>;
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return (
+        <div>
+          <div className="location-error">
+            <h1>Error: {this.state.errorMessage}</h1>
+          </div>
+          <div className="error-message">
+            <h1> Cmon man you gotta give me location permission</h1>
+          </div>
+        </div>
+      );
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Loading message="Waiting for location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
